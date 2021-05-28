c84c506d8786222284cbd2bb2c1276d701d8cd93
Dennis <dennis@Air-von-Dennis.fritz.box>
2021-05-28 17:38:13 +0200

    changed eslint config

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ module.exports = {
     globals: {
         MyGlobal: true
     },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "prettier"],
+    root: true,
     rules: {
-        semi: ['error', 'always']
-    }
+        "prettier/prettier": "error",
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/no-unused-vars": "error"
+    },
+    ignorePatterns: ["**/*.d.ts", "**/*.js"]
 };
