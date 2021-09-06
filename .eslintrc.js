eb7749cc8dd4feaf6c781d932cfe042b8c248563
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2021-09-06 15:50:38 +0200

    Update rules and move to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    extends: "airbnb-typescript-prettier",
+    ignorePatterns: ["**/*.d.ts", "**/*.js"],
+    rules: {
+        "import/prefer-default-export": "off",
+        "sort-imports": [
+            "warn",
+            {
+                ignoreMemberSort: false,
+            },
+        ],
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/explicit-member-accessibility": [
+            "warn",
+            {
+                "accessibility": "explicit",
+            },
+        ],
+        "prettier/prettier": [
+            "warn",
+            {
+                printWidth: 120,
+                semi: true,
+                singleQuote: true,
+                tabWidth: 4,
+            },
+        ],
+    },
+};
