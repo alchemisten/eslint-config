9fc85eac52335639fb1bd5d89c7b72eb8c2f671d
Viktor Spadi <viktor.spadi@alchemisten.ag>
2021-09-22 12:54:29 +0200

    remove no-param-reassign rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
         "no-useless-constructor": "off",
         "no-restricted-syntax": "off",
         "no-cond-assign": "off",
+        "no-param-reassign": "off",
         "consistent-return": "off",
         "import/prefer-default-export": "off",
         "sort-imports": [
