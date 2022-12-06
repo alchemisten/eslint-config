d9cd95f7faeb3fb62d116e1a48cbe72d357abd04
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2022-12-06 14:25:05 +0100

    fix: Change tab width to 2

--- example/typescript-class.ts
+++ example/typescript-class.ts
@@ -1,18 +1,18 @@
 export class TheClass {
-    private sum: number;
+  private sum: number;
 
-    public constructor() {
-        this.doSomething();
-    }
+  public constructor() {
+    this.doSomething();
+  }
 
-    public static getMeSomeStrings(): string[] {
-        return ['a', 'b', 'c'];
-    }
+  public static getMeSomeStrings(): string[] {
+    return ['a', 'b', 'c'];
+  }
 
-    private doSomething(): void {
-        this.sum = 0;
-        for (let i = 0; i < 100; i += 1) {
-            this.sum += i;
-        }
+  private doSomething(): void {
+    this.sum = 0;
+    for (let i = 0; i < 100; i += 1) {
+      this.sum += i;
     }
+  }
 }
--- example/typescript.ts
+++ example/typescript.ts
@@ -1,41 +1,41 @@
 export const bob = (a: string): string => {
-    return a;
+  return a;
 };
 
 export interface Foo {
-    a: number;
-    b: number;
-    c: number;
-    d: string;
+  a: number;
+  b: number;
+  c: number;
+  d: string;
 }
 
 export const carl = (c: number): void => {
-    const stuff = 0;
-    const foo: Foo = {
-        a: 0,
-        b: 9,
-        c: 12,
-        d: 'a Code',
-    };
+  const stuff = 0;
+  const foo: Foo = {
+    a: 0,
+    b: 9,
+    c: 12,
+    d: 'a Code',
+  };
 
-    Object.keys(foo).forEach((key) => {
-        foo[key] += `-${stuff}-${c}`;
-    });
+  Object.keys(foo).forEach((key) => {
+    foo[key] += `-${stuff}-${c}`;
+  });
 };
 
 export interface NamedObject {
-    name: string;
+  name: string;
 }
 
 /* This example is deliberately verbose to demonstrate consistent-return.
  * Remove the return in the last line to see the error.
  */
 export const findByName = (list: NamedObject[], name: string): NamedObject | undefined => {
-    for (const item of list) {
-        if (item.name === name) {
-            return item;
-        }
+  for (const item of list) {
+    if (item.name === name) {
+      return item;
     }
+  }
 
-    return undefined;
+  return undefined;
 };
